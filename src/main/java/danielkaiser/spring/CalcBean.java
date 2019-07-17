9b2d5aab905f9520b9781fe6b933303aaba6db17
Daniel Kaiser <danielkaiser@fastmail.com>
2019-07-17 21:58:10 +0200

    Cleaned up code and dependencies

--- src/main/java/danielkaiser/spring/CalcBean.java
+++ src/main/java/danielkaiser/spring/CalcBean.java
@@ -2,24 +2,25 @@ package danielkaiser.spring;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 @Service
 public class CalcBean {
 
     String calculate(int number) {
 
-        String result = "Die Teiler sind: ";
-        boolean vorher = false;
+        final var results = new ArrayList<Integer>();
+
         for (int i = 2; i <= number; i++) {
             while (number % i == 0) {
                 number /= i;
-                if (vorher)
-                    result += ", ";
-                vorher = true;
-                result += String.valueOf(i);
+
+                results.add(i);
             }
         }
-        result += ".";
 
-        return result;
+
+        return "Die Teiler sind: " + results.stream().map(Object::toString).collect(Collectors.joining(", ")) + ".";
     }
 }
